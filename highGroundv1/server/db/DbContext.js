ff6f788903bf9f6b639e8987819eb96f28d3ca84
Haven Fricke <robslaven@live.com>
2022-05-03 14:02:21 -0600

    db model setup

--- highGroundv1/server/db/DbContext.js
+++ highGroundv1/server/db/DbContext.js
@@ -1,11 +1,21 @@
 import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
+import { CartItemSchema } from "../models/CartItem";
+import { EventSchema } from "../models/Event";
+import { MailRecepientSchema } from "../models/MailRecepient";
+import { StoreMerchSchema } from "../models/StoreMerch";
+import { StoreTicketSchema } from "../models/StoreTicket";
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
+  CartItem = mongoose.model('CartItem', CartItemSchema)
+  Event = mongoose.model('Event', EventSchema)
+  MailRecepient = mongoose.model('MailRecepient', MailRecepientSchema)
+  StoreMerch = mongoose.model('StoreMerch', StoreMerchSchema)
+  StoreTicket = mongoose.model('StoreTicket', StoreTicketSchema)
 }
 
 export const dbContext = new DbContext()
